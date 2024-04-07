322974b0b0dc231f4f91f21853d212c10e57608f
Sean Reed <sean.reed@gmail.com>
2024-04-07 13:54:49 -0400

    adds src dir to path so can run the azure config files

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import json
+import os
+from pathlib import Path
+
+LOCAL_CONFIG_DIR = Path.home() / "WAB-config"
+
+def run_azure_config(config_dir):
+    all_config_file_path = config_dir / "allconfig.json"
+    config = {}
+    with open(all_config_file_path) as json_config:
+        config.update(json.load(json_config))
+        for k in config:
+            os.environ[k] = config[k]
